6d66d789ca54fc57b026b344edbaa81dcb44362d
quhao <quhaowk@163.com>
2023-12-27 19:45:07 +0800

    dev

---
+++ src/Util.php
@@ -0,0 +1,34 @@
+<?php
+namespace Haoa\Util;
+
+class Util
+{
+    /**
+     * @return bool
+     */
+    protected static function isCoroutine(): bool
+    {
+        if (!class_exists(\Swoole\Coroutine::class)) {
+            return false;
+        }
+        if (\Swoole\Coroutine::getCid() != -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public static function stackDefer(?\SplStack &$context, callable $callback): void
+    {
+        $context ??= new class() extends \SplStack {
+            public function __destruct()
+            {
+                while ($this->count() > 0) {
+                    \call_user_func($this->pop());
+                }
+            }
+        };
+
+        $context->push($callback);
+    }
+
+}
